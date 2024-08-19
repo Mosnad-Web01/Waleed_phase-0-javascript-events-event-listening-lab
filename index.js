9d01ba8cfcf8583cae1086b84383e9ba350e82d4
waleed7333 <waleed733150@gmail.com>
2024-08-19 13:24:57 +0300

    update JS file

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 function addingEventListener() {
+  const input = document.getElementById("button");
+
+  // Anonymous function for the click event
+  input.addEventListener("click", function () {
+    alert("I was clicked!");
+  });
+
+  // Named function for the click event
+  function clickAlert() {
+    alert("I was clicked!");
+  }
+
+  input.addEventListener("click", clickAlert);
 }
+
+// Call the function to set up the event listeners
+addingEventListener();
